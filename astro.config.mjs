b69c0782e8ef0abf6354723f634552c8262dec15
Michal Gazda <michal@gazda.info>
2026-09-13 15:14:28 +0200

    Fix base path and canonical URLs for high-press.pl repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://michalgazda.github.io',
-  base: '/high-pres.pl/',
+  base: '/high-press.pl/',
   output: 'static',
   outDir: './dist',
 });
