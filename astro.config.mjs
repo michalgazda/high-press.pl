3ec122b12249b8c4455dde76599240003b4561ee
Michal Gazda <michal@gazda.info>
2026-09-13 15:22:10 +0200

    Fix base path to high-pres.pl (single s)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://michalgazda.github.io',
-  base: '/high-press.pl/',
+  base: '/high-pres.pl/',
   output: 'static',
   outDir: './dist',
 });
